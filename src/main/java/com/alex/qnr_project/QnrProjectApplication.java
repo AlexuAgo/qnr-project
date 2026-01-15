41815cb77d14e6857a5b77b440b3517327d38d5d
Alex Ago <alexago192599@gmail.com>
2026-01-15 16:49:59 +0200

    First Spring Boot project commit

---
+++ src/main/java/com/alex/qnr_project/QnrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alex.qnr_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnrProjectApplication.class, args);
+    }
+
+}
